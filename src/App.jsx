f12dc6bc656d22e216d3f421cc7cacda4040d4c2
SayuryKato <111785557+SayuryKato@users.noreply.github.com>
2023-10-02 20:08:11 +0000

    Criando pasta components

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
-
-        <div className="card__tags">
+        <div className="tags">
           <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra-137</div>
+          <div className="tag">Espécies: Humano</div>
+          <div className="tag">Origem: Terra C-137</div>
         </div>
-
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez"/>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
       </div>
     </>
   )
