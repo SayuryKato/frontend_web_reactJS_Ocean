d5e70bd7ac49c4d8a1a6e7fdf0a94ced55fac693
SayuryKato <111785557+SayuryKato@users.noreply.github.com>
2023-10-03 00:20:50 +0000

    Atualização da pasta components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,13 @@
 import "./Card.css"
 
 export default function Card() {
-    return <div>Card</div>
+    return <div className="card">
+    <h2>Rick Sanchez</h2>
+    <div className="tags">
+      <div className="tag">Status: Vivo</div>
+      <div className="tag">Espécies: Humano</div>
+      <div className="tag">Origem: Terra C-137</div>
+    </div>
+    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
+  </div>
 }
